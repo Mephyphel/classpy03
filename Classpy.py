d404f1a0d7ad160db84e9d2ef2cceeab453f5a27
Mephyphel <89886422+Mephyphel@users.noreply.github.com>
2021-10-12 19:49:15 -0500

    Update Classpy.py

--- Classpy.py
+++ Classpy.py
@@ -1,3 +1,16 @@
+"""
 x=5
 x=x*2
 print (x);
+
+"""
+luis = 5
+pedro = 10
+#quiero que luis tenga la misma cantidad de dinero que pedro
+luis = pedro
+
+print("luis tiene : ",luis," soles");
+
+#quiero que luis tenga 10 soles mas de lo que hoy tiene
+
+luis += 10
